88e481196d7bbad9cbe7dac724213c158b366693
Henry <Hendrikvanbeersel@hotmail.com>
2023-06-30 18:22:15 +0200

    pwa icons(does not work)

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'Portfolio Hendrik Van Beersel',
+        short_name: 'Portfolio HVB',
+        theme_color: '#024b30',
+        icons: [
+          {
+            src: './icons/pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './icons/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
