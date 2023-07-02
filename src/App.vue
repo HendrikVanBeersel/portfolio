4d36ccb5477ab1e9fae4960eb8c2d3a1d1c56d0e
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-02 20:56:06 +0200

    getting current page value

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const currentRoute = ref<string>('home')
+watch(useRoute(), (to, from) => {
+  try {
+    currentRoute.value = to.name!.toString()
+  } catch (e) {
+    currentRoute.value = ''
+  }
+})
 </script>
 
 <template>
-  <header>
+  <header class="p-8">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="p-4">
+      <h1>Hendrik Van Beersel</h1>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="flex justify-center">
+        <RouterLink class="p-2" to="/">Home</RouterLink>
+        <RouterLink class="p-2" to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { computed, createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
