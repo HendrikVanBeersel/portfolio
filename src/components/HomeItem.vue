f6e7c7b7ba2806342508f0523decbcf8fe28b455
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-03 00:09:15 +0200

    preparing home screen items

---
+++ src/components/HomeItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <slot name="icon"></slot>
+  </div>
+  <div>
+    <h3>
+      <slot name="heading"></slot>
+    </h3>
+    <slot name="text"></slot>
+  </div>
+</template>
