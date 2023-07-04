4e5edde9f0bbac1ab0ed794dd1e6ee01fecad7c3
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-04 16:58:12 +0200

    the main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: { primary: '#024b30', secundary: '#30024B', tertiary: '#4B3002' },
+      borderWidth: { 3: '3px' }
+    }
   },
   plugins: []
 }
