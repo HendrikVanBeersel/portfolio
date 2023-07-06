76f063bc29a60144159f2dac8bf3fcf995e35a16
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-06 14:01:01 +0200

    education view

--- src/components/HomeItem.vue
+++ src/components/HomeItem.vue
@@ -4,15 +4,29 @@ defineProps<{ routerLinkName: string }>()
 <template>
   <div class="relative flex flex-row justify-start items-center">
     <div class="absolute left-[1px] border-r-2 border-tertiary h-40 w-[4.166667%]"></div>
-
+    <h3
+      v-if="!routerLinkName"
+      class="z-10 self-center basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
+    >
+      <slot name="icon"></slot>
+    </h3>
     <button
+      v-else
       @click="$router.push({ name: routerLinkName })"
       class="z-10 self-center basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
     >
       <slot name="icon"></slot>
     </button>
     <div class="basis-11/12 self-center">
+      <h2
+        v-if="!routerLinkName"
+        class="pl-4 mb-4 text-xl underline underline-offset-8 decoration-8 decoration-secundary"
+      >
+        <slot name="heading"></slot>
+      </h2>
+
       <button
+        v-else
         @click="$router.push({ name: routerLinkName })"
         class="pl-4 mb-4 text-xl underline underline-offset-8 decoration-8 decoration-secundary"
       >
---
+++ src/views/EducationView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import HomeItem from '@/components/HomeItem.vue'
+</script>
+<template>
+  <main>
+    <HomeItem routerLinkName="">
+      <template #icon> <p class="p-2">2023</p> </template>
+      <template #heading>Graduate Programing (Cum Laude)</template>
+      <template #text class="">
+        <p>
+          I graduated with a cum laude distinction. I learned a lot about programming and the scrum
+          process. I also learned how to work in a team and how to communicate effectively.
+        </p>
+      </template>
+    </HomeItem>
+    <HomeItem routerLinkName="">
+      <template #icon> <p class="p-2">2023</p> </template>
+      <template #heading>
+        <p>
+          Certified Scrum Master
+          <img class="inline h-12" src="@/assets/seal-csm.png" alt="csm image" />
+        </p>
+      </template>
+
+      <template #text class=""> I took a scrum master course in june 2023. </template>
+    </HomeItem>
+  </main>
+</template>
