aa36782d4baecdc61be404fa663fd68795c63d76
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-06 13:13:24 +0200

    skills view

---
+++ src/components/icons/programming/IconPython.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600">
+    <title>language-python</title>
+    <path
+      d="M 300,190 H 120 c 0,0 -50,0 -50,110 0,110 50,110 50,110 h 50 c 0,0 0,-10 0,-50 0,-40 30,-70 70,-70 40,0 89.86755,0 119.93377,0 C 390,290 410,270 410.41391,239.51987 410.82781,209.03974 410,120 410,120 c 0,0 0,-50 -110,-50 -110,0 -110,50 -110,50 v 50 h 110 z"
+      inkscape:connector-curvature="0"
+      style="fill: #387eb8; fill-opacity: 1"
+    />
+    <path
+      d="m 300,410 h 180 c 0,0 50,0 50,-110 0,-110 -50,-110 -50,-110 h -50 c 0,0 0,10 0,50 0,40 -30,70 -70,70 -40,0 -90,0 -120,0 -30,0 -50,20 -50,50 0,30 0,120 0,120 0,0 0,50 110,50 110,0 110,-50 110,-50 V 430 H 300 Z"
+      inkscape:connector-curvature="0"
+      style="fill: #ffc331; fill-opacity: 1"
+    />
+    <circle cx="240" cy="120" r="20" style="fill: #ffffff; fill-opacity: 1" />
+    <circle cx="360" cy="480" r="20" style="fill: #ffffff; fill-opacity: 1" />
+  </svg>
+</template>
