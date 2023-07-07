b92e001d55525fb9beb420ca2287a23d19ee99e4
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-07 18:51:28 +0200

    contact functional working

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,58 +1,63 @@
-<script>
+<script setup lang="ts">
 import emailjs from 'emailjs-com'
+import { ref } from 'vue'
 
-export default {
-  data() {
-    return {
-      formData: {
-        name: '',
-        email: '',
-        message: ''
-      }
-    }
-  },
-  methods: {
-    submitForm() {
-      const templateParams = {
-        from_name: this.formData.name,
-        from_email: this.formData.email,
-        message: this.formData.message
-      }
+const email = ref<string>('')
+const name = ref<string>('')
+const message = ref<string>('')
+const buttonOn = ref<boolean>(true)
 
-      emailjs
-        .send('<service_dff52cf>', '<YOUR_TEMPLATE_ID>', templateParams, '<YOUR_USER_ID>')
-        .then(() => {
-          alert('Email sent successfully!')
-          this.resetForm()
-        })
-        .catch((error) => {
-          console.error('Error sending email:', error)
-        })
+function sendEmail(infoToSend: Record<string, string>) {
+  emailjs.send('service_dff52cf', 'template_81r8e2l', infoToSend, 'cAFPwog3gCQZfpObs').then(
+    (result) => {
+      console.log('SUCCESS!', result.text)
+      alert('Message sent successfully')
     },
-    resetForm() {
-      this.formData.name = ''
-      this.formData.email = ''
-      this.formData.message = ''
+    (error) => {
+      console.log('FAILED...', error.text)
+      alert('Message failed to send')
     }
+  )
+  buttonOn.value = true
+}
+
+function submitForm() {
+  buttonOn.value = false
+  const emailValue = email.value.trim()
+  const nameValue = name.value.trim()
+  const messageValue = message.value.trim()
+
+  if (emailValue === '' || nameValue === '' || messageValue === '') {
+    alert('Please fill out all fields')
+  } else if (!isValidEmail(emailValue)) {
+    alert('Please enter a valid email address')
+  } else if (messageValue.length < 10) {
+    alert('Please enter a message with at least 10 characters')
+  } else {
+    const InfoToSend = {
+      user_name: nameValue,
+      message: messageValue,
+      user_email: emailValue
+    }
+    sendEmail(InfoToSend)
   }
+}
+
+function isValidEmail(email: string) {
+  // Use a regular expression to validate the email format
+  const emailPattern = /^\S+@\S+\.\S+$/
+  return emailPattern.test(email)
 }
 </script>
 <template>
   <main>
-    <div><h2>Contact info</h2></div>
-    <form @submit.prevent="submitForm">
-      <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="formData.name" required />
-      </div>
-      <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="formData.email" required />
-      </div>
-      <div>
-        <label for="message">Message:</label>
-        <textarea id="message" v-model="formData.message" required></textarea>
-      </div>
+    <form id="form" ref="form" class="flex flex-col" @submit.prevent="submitForm">
+      <label>Name</label>
+      <input type="text" v-model="name" name="user_name" />
+      <label>Email</label>
+      <input type="email" v-model="email" name="user_email" />
+      <label>Message</label>
+      <textarea v-model="message" name="message"></textarea>
       <button type="submit">Submit</button>
     </form>
   </main>
