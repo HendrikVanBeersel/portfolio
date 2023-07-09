0f26bcf653971ce0b727ea728b9d9efd375e2c5a
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-10 01:23:33 +0200

    projects started

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -3,43 +3,47 @@ import IconBike from '@/components/icons/IconBike.vue'
 import IconCards from '@/components/icons/IconCards.vue'
 import IconSourceBranch from '@/components/icons/IconSourceBranch.vue'
 import IconSwim from '@/components/icons/IconSwim.vue'
-      //anyway  anyhow instead off but
-
+//anyway  anyhow instead off but
 </script>
 <template>
   <main class="m-8 text-lg lg:w-10/12 xl:w-8/12 2xl:w-7/12">
     <ul class="space-y-4">
       <li>
-        <p>Hello I am Hendrik and here is a bit about me as a person</p>
+        <p>Hello, I am Hendrik and here you can read more about my interests and motivations:</p>
         <IconBike class="float-left h-12 w-12 fill-primary" />
         <p>
-          I Go biking almost every weekend with my friends. I prefer it the as my main mode of
-          transportation as well. In my Internship I went by bike a few times a week.
+          I go biking almost every weekend with my friends. I prefer biking as my main mode of
+          transportation. During my Internship I went to work by bike several times a week.
         </p>
         <IconSwim class="float-right h-20 w-20 fill-primary" />
         <p>
-          I was really athletic when I was younger. when I was around 18 I became provincial
-          champion in breastroke swimming (100m,50m). I als earned a lange wapper ( you need to be 1
-          year the fastest in your category)
+          I am also highly passionate about swimming. When I was around 18 years old, I became the
+          provincial champion in the 100m and 50 m breastroke. Another merit I earned was a 'lange
+          wapper',this meant that I was the fastest swimmer in my category for an entire year.
         </p>
       </li>
       <li>
         <p>
-          In my free time I also enjoy spending time with friends going out, visitinge places etc.
-          But I really enjoy playing boardgames especially ones where you can try new tactics and
-          change it up every time.
+          In my free time I enjoy spending time with friends, going out, visiting places etc.
+          Playing boardgames, especially ones where you can try new tactics and change it up every
+          time, is one of my favorite 'rainy day' pastimes.
           <IconCards class="float-left h-20 w-20 fill-primary" />
-          It is not that I need to win to have a good time (I do find it still fun of course) But I
-          really enjoy to see ways of play that are intresting and diffrent.
+          I don't need to win to have a good time (although I won't pass up to chance to win of
+          course) but I really enjoy to see ways of play that are interesting and different.
         </p>
       </li>
       <li>
         <IconSourceBranch class="float-right h-16 w-16 fill-primary" />
         <p>
-          I also enjoy programming in my free time and learning new technologies every time. I am
-          currently trying to work with azure, making my own api,creating a place to connect safely
-          to a home server and I would like to see what it all has in store. If You want to see more
-          You can always look at my <span class="underline">projects</span>.
+          Programming and learning new technologies has a way of finding me in my free time as well.
+          I am currently trying to work with azure, making my own api, creating a place to connect
+          safely to a home server and I would like to see what it all has to offer. If you want to
+          learn more about my individual projects, you can find more information at the .<button
+            class="inline-block underline"
+            @click="$router.push({ name: 'projects' })"
+          >
+            'projects tab'</button
+          >.
         </p>
       </li>
     </ul>
