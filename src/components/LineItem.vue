a8cd658d4fa552b582daa32bc8383a9524f0d658
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-09 18:57:31 +0200

    view for tablet

--- src/components/LineItem.vue
+++ src/components/LineItem.vue
@@ -2,20 +2,20 @@
 defineProps<{ routerLinkName: string }>()
 </script>
 <template>
-  <div class="relative flex flex-row justify-start items-center ml-1 sm:ml-1">
+  <div class="relative flex flex-row justify-start items-center ml-1 ">
     <div
-      class="absolute left-[1px] border-r-2 border-tertiary h-60 xl:h-40 w-[6.25%] xl:w-[4.166667%]"
+      class="absolute left-[1px] border-r-2 border-tertiary h-60 sm:h-40 w-[6.25%] sm:w-[4.166667%]"
     ></div>
     <h3
       v-if="!routerLinkName"
-      class="z-10 self-center basis-[12.5%] xl:basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
+      class="z-10 self-center basis-[12.5%] sm:basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
     >
       <slot name="icon"></slot>
     </h3>
     <button
       v-else
       @click="$router.push({ name: routerLinkName })"
-      class="z-10 self-center basis-[12.5%] xl:basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
+      class="z-10 self-center basis-[12.5%] sm:basis-1/12 rounded-full border-2 border-tertiary bg-slate-950"
     >
       <slot name="icon"></slot>
     </button>
