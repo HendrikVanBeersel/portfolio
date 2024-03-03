d843f47f9d7dc47a5aef602267202d653c528323
LavaHenry <hendrikvanbeersel@hotmail.com>
2024-03-03 19:05:46 +0100

    Aexis Experience

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -4,16 +4,26 @@ import ExperienceItem from '@/components/LineItem.vue'
 <template>
   <main>
     <ExperienceItem class="mt-10" routerLinkName="">
-      <template #icon> <p class="p-2 w-24">2022-2023</p> </template>
+      <template #icon> <p class="w-44 p-2">Oktober 2023 - now</p> </template>
+      <template #heading>junior consultant at <a href="https://aexis.com/">AEXIS</a></template>
+      <template #text>
+        <p class="mr-10">
+          I'm currently working at AEXIS I'm busy with the finhosta aplication and learning to work
+          with the api of tm1 and cognos.
+        </p>
+      </template>
+    </ExperienceItem>
+    <ExperienceItem class="mt-10" routerLinkName="">
+      <template #icon> <p class="w-44 p-2">schoolyear 2022-2023</p> </template>
       <template #heading
         >Internship with <a href="https://but.digital/be-en/">BUT digital</a>
       </template>
-      <template #text class="">
-        <p>
+      <template #text>
+        <p class="mr-10">
           I have really good memories of this experience and learned a lot. After this internship, I
           felt ready to start working as a programmer.
         </p>
-        <p>References available upon request.</p>
+        <p class="underline">References available upon request.</p>
       </template>
     </ExperienceItem>
   </main>
