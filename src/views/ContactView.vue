bfca0ff30d4a4e503452d82ba94f9b99f71d2bcc
LavaHenry <hendrikvanbeersel@hotmail.com>
2024-03-17 21:03:09 +0100

    contact form fixed

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,80 +1,73 @@
 <script setup lang="ts">
-import emailjs from '@emailjs/browser'
-import { ref, watch } from 'vue'
-import IconSpin from '@/components/icons/animatedIcons/IconSpin.vue'
+import { ref } from 'vue'
 
-const email = ref<string>('')
-const name = ref<string>('')
-const message = ref<string>('')
-const buttonIsDisabled = ref<boolean>(false)
+const email = ref('')
+const text = ref('')
+const modal = ref(false)
+const modaltext = ref('error')
 
-function sendEmail(infoToSend: Record<string, string>) {
-  emailjs.send('service_dff52cf', 'template_81r8e2l', infoToSend, 'cAFPwog3gCQZfpObs').then(
-    (result) => {
-      console.log('SUCCESS!', result.text)
-      alert('Message sent successfully')
-    },
-    (error) => {
-      console.log('FAILED...', error.text)
-      alert('Message failed to send')
+async function SendEmail() {
+  console.log('email', email.value)
+  console.log('text', text.value)
+  await fetch('https://formspree.io/f/xvoebzzg', {
+    method: 'Post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email: email.value, text: text.value })
+  }).then((response) => {
+    if (response.ok) {
+      console.log('succes')
+      modaltext.value = 'maile sent succesfully'
+      modal.value = true
+    } else {
+      console.log('fail')
+      modaltext.value = 'Service is currently not working\nPlease contact me with linkedin'
+      modal.value = true
     }
-  )
-  buttonIsDisabled.value = false
+  })
 }
-
-function submitForm() {
-  buttonIsDisabled.value = true
-  const emailValue = email.value.trim()
-  const nameValue = name.value.trim()
-  const messageValue = message.value.trim()
-
-  if (emailValue === '' || nameValue === '' || messageValue === '') {
-    alert('Please fill out all fields')
-    buttonIsDisabled.value = false
-  } else if (!isValidEmail(emailValue)) {
-    alert('Please enter a valid email address')
-    buttonIsDisabled.value = false
-  } else if (messageValue.length < 10) {
-    alert('Please enter a message with at least 10 characters')
-    buttonIsDisabled.value = false
-  } else {
-    const InfoToSend = {
-      user_name: nameValue,
-      message: messageValue,
-      user_email: emailValue
-    }
-    sendEmail(InfoToSend)
-  }
-}
-
-function isValidEmail(email: string) {
-  // Use a regular expression to validate the email format
-  const emailPattern = /^\S+@\S+\.\S+$/
-  return emailPattern.test(email)
-}
-watch(buttonIsDisabled, (newValue) => {
-  if (newValue) {
-    submitForm()
-  } else {
-  }
-})
 </script>
 <template>
-  <main class="w-full p-10 sm:p-16 md:p-20 flex flex-col">
-    <label>Name:</label>
-    <input class="text-black" type="text" v-model="name" name="user_name" />
-    <label class="mt-4">Email:</label>
-    <input class="text-black" type="email" v-model="email" name="user_email" />
-    <label class="mt-4">Message:</label>
-    <textarea class="text-black" v-model="message" name="message"></textarea>
+  <form class="flex w-4/5 flex-col sm:w-3/5 xl:w-1/2" @submit.prevent>
+    <label for="email">email: </label>
+    <input
+      v-model="email"
+      id="email"
+      type="email"
+      name="email"
+      placeholder="your email"
+      required
+      class="mb-8 p-2 text-black"
+    />
+
+    <label for="message">message: </label>
+    <textarea
+      v-model="text"
+      type="text"
+      id="message"
+      name="message"
+      placeholder="your message"
+      required
+      class="mb-8 p-2 text-black"
+    ></textarea>
+
     <button
-      :disabled="buttonIsDisabled"
-      class="mt-4 border-4 border-primary rounded-lg bg-primary text-xl inline-flex items-center justify-center"
+      @click="SendEmail"
       type="submit"
-      v-on:click="buttonIsDisabled = true"
+      class="self-center rounded-lg border-8 border-primary px-5 py-1 hover:border-secundary"
     >
-      <IconSpin v-show="buttonIsDisabled" />
-      <span v-show="!buttonIsDisabled">Send</span>
+      submit
     </button>
-  </main>
+    <div
+      v-if="modal"
+      class="absolute z-10 flex flex-col self-center border-8 border-primary bg-white object-center p-5 text-black"
+    >
+      <p class="whitespace-pre">{{ modaltext }}</p>
+      <button
+        class="rounded-lg border-8 border-primary bg-black px-5 py-1 text-white hover:border-secundary hover:bg-secundary"
+        @click="modal = false"
+      >
+        Close
+      </button>
+    </div>
+  </form>
 </template>
